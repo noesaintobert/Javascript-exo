3e7479ade869ad9fe20c1a72b7724a04e49bbd52
Noé Saintobert <saintobert.noe@gmail.com>
2023-03-22 09:58:22 +0100

    javascript

---
+++ exercice.js
@@ -0,0 +1,37 @@
+/* exercice 1 */
+var age =25;
+
+/* exercice 2 */
+var nom = "Jean";
+
+/* exercice 3 */
+var somme = 10+13;
+
+/* exercice 4 */
+var chaine ="jesuisunechaine"
+var resultChaine = chaine.toUpperCase();
+console.log(resultChaine)
+
+/* exercice 5 */
+let tableau =[1,2,3,4,5]
+var resultTableau = tableau.length;
+console.log(resultTableau);
+
+/* exercice 6 */
+let objet={nom, age}
+console.log(objet.nom);
+
+/* exercice 7 */
+var nombre = 11.03
+console.log(nombre.toFixed());
+
+/* exercice 8 */
+const date = new Date('22 Mars, 2023');
+console.log(date.getMonth());
+
+/* exercice 9 */
+let tableau2 = ["Je","Suis","Une","Chaîne"]
+console.log(tableau2.join())
+
+
+
