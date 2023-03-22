b8700b7e25fb47b30e3d2e084a9a0123d5f31b86
Noé Saintobert <saintobert.noe@gmail.com>
2023-03-22 10:13:19 +0100

    Update
    10H13

--- exercice.js
+++ exercice.js
@@ -1,11 +1,15 @@
 /* exercice 1 */
 var age =25;
+console.log(age);
 
 /* exercice 2 */
 var nom = "Jean";
+console.log(nom);
 
 /* exercice 3 */
 var somme = 10+13;
+console.log(somme);
+
 
 /* exercice 4 */
 var chaine ="jesuisunechaine"
